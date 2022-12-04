cd44ceb77f7a2b27d653d6fdeb91b62d36fe80c1
Alexey Larionov <alxs.larionov@gmail.com>
2022-12-04 10:54:49 +0300

    Task 4

---
+++ q4b.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <unordered_map>
+#include <set>
+
+struct Range {
+   long long begin, end;
+
+   Range& operator=(const Range& other) = default;
+};
+
+long long read_and_parse_int(std::istream& stream, char separator) {
+   static std::string input;
+   std::getline(stream, input, separator);
+   return std::stoi(input);
+}
+
+int main() {
+   std::ifstream input_file("data/q4.txt");
+   long long fully_covered_count = 0;
+   while(!input_file.eof()) {
+      Range elf1, elf2;
+      elf1.begin = read_and_parse_int(input_file, '-');
+      elf1.end   = read_and_parse_int(input_file, ',');
+      elf2.begin = read_and_parse_int(input_file, '-');
+      elf2.end   = read_and_parse_int(input_file, '\n');
+      if (elf2.begin < elf1.begin) {
+         // swap to make sure elf1 atleast range precedes elf2 range
+         Range tmp = elf2;
+         elf2 = elf1;
+         elf1 = tmp;
+      }
+      if (elf2.begin > elf1.end) {
+         std::cout << "no overlap: " << elf1.begin << '-' << elf1.end << ' ' << elf2.begin << '-' << elf2.end << '\n';
+      } else {
+         std::cout << "overlap: " << elf1.begin << '-' << elf1.end << ' ' << elf2.begin << '-' << elf2.end << '\n';
+         ++fully_covered_count;
+      }
+   }
+   std::cout << fully_covered_count;
+   return 0;
+}
